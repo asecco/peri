701c82be585c9cefe18ccd5c928022c9286c8346
Andrew Secco <asecco99@gmail.com>
2022-07-18 13:37:01 -0400

    Nextjs server side rendering

---
+++ components/Results.js
@@ -0,0 +1,9 @@
+function Results({requests}) {
+  return (
+    <div>
+      <h1>Results</h1>
+    </div>
+  )
+}
+
+export default Results;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home({results}) {
+  console.log(results)
   return (
     <div>
       <Head>
@@ -13,6 +16,18 @@ export default function Home() {
 
       <Header />
       <Nav />
+      <Results results={results} />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre
+  const req = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.Trending.url}`).then((res) => res.json())
+
+  return {
+    props: {
+      results: req.results,
+    }
+  }
 }
