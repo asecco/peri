3392135bf090d81bd345c3c56148b0bc99224626
Andrew Secco <asecco99@gmail.com>
2022-07-18 16:39:04 -0400

    Added footer component for changing pages

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import {
+    ArrowCircleRightIcon,
+    ArrowCircleLeftIcon,
+} from '@heroicons/react/outline';
+import HeaderItem from './HeaderItem';
+
+function Footer() { 
+    return (
+        <div className='flex flex-row sm:flex-row mb-5 justify-between items-center h-auto'>
+            <HeaderItem title='Previous' Icon={ArrowCircleLeftIcon} />
+            <HeaderItem title='Next' Icon={ArrowCircleRightIcon} />
+        </div>
+    );
+}
+
+export default Footer;
--- components/Results.js
+++ components/Results.js
@@ -1,13 +1,18 @@
 import Thumbnail from "./Thumbnail";
 import FlipMove from "react-flip-move";
+import Footer from "./Footer";
 
 function Results({results}) {
   return (
-    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-4 3xl:flex flex-wrap justify-center">
-      {results.map((result) => (
-        <Thumbnail key={result.id} result={result} />
-      ))}
-    </FlipMove>
+    <div>
+        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-6 3xl:grid-cols-8">
+        {results.map((result) => (
+            <Thumbnail key={result.id} result={result} />
+        ))}
+        </FlipMove>
+        
+        <Footer />
+    </div>
   );
 }
 
