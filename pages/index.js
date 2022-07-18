6542b9d00aaf477008a184c8583adb9e35f30af5
Andrew Secco <asecco99@gmail.com>
2022-07-18 11:09:58 -0400

    Created react/next/tailwind boilerplate

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Peri</title>
+        <meta name="description" content="Peri" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
