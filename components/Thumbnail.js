5e6f597f020c3098442de8c8bd93c76869e4f37c
Andrew Secco <asecco99@gmail.com>
2022-07-18 15:22:38 -0400

    Added flipmove for genre transitions

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,15 +1,16 @@
 import Image from 'next/image';
+import {forwardRef} from 'react';
 
-function Thumbnail({result}) {
+const Thumbnail = forwardRef(({result}, ref) => {
     const BASE_URL = 'https://image.tmdb.org/t/p/original/';
     return (
-        <div className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
-            <Image layout='responsive' src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} height={1080} width={1920} /> {/* layout='fill', fixed, intrinsic */}
-            <div className='p-2'>
+        <div ref={ref} className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
+            <Image layout='responsive' src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} height={1080} width={1920} />
+            <div className='p-2 text-center'>
                 <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold truncate max-w-md'>{result.title || result.original_name}</h2>
             </div>
         </div>
     )
-}
+})
 
 export default Thumbnail;
