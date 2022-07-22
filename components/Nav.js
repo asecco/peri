f936d8b5ee3885f272506bcf85f322b7334385c5
Andrew Secco <asecco99@gmail.com>
2022-07-22 02:24:01 -0400

    Updated background color

--- components/Nav.js
+++ components/Nav.js
@@ -11,7 +11,7 @@ function Nav() {
                     <h2 key={key} onClick={() => router.push(`/?genre=${key}`)} className="last:pr-10 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-red-400 active:text-red-500">{title}</h2>
                 ))}
             </div>
-            <div className="absolute top-0 right-0 bg-gradient-to-l from-[#3A45B1] h-10 w-1/12" />
+            <div className="absolute top-0 right-0 bg-gradient-to-l from-[#4a5c5b] h-10 w-1/12" />
         </nav>
     );
 }
