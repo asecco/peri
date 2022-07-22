a813348f69c9c0ebc3f691a986153be7bc886af1
Andrew Secco <asecco99@gmail.com>
2022-07-21 20:19:50 -0400

    Added cast component for displaying cast members

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,13 +1,19 @@
 import Image from 'next/image';
 import {forwardRef} from 'react';
 import React, {useState} from 'react';
+import {useRouter} from "next/router";
 
 const Thumbnail = forwardRef(({result}, ref) => {
     const BASE_URL = 'https://image.tmdb.org/t/p/original/';
     const [hoverInfo, setHoverInfo] = useState({display: 'none'});
 
+    const router = useRouter();
+    const routeQuery = () => {
+        router.push({pathname: '/MovieInfo', query: result});
+    }
+
     return (
-        <div ref={ref} className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50' onMouseEnter={e => { setHoverInfo({display: 'block'});}} onMouseLeave={e => { setHoverInfo({display: 'none'})}}>
+        <div onClick={routeQuery} ref={ref} className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50' onMouseEnter={e => { setHoverInfo({display: 'block'});}} onMouseLeave={e => { setHoverInfo({display: 'none'})}}>
             <Image className='group-hover:opacity-30' layout='responsive' src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} alt='' height={1080} width={1920}/>
             <div style={hoverInfo} layout='responsive' className="absolute bottom-20 p-4 group-hover:font-bold text-base">
                 <p className='line-clamp-9'>{result.description || result.overview}</p>
