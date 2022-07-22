b8aa79c3657ee2e00374f20fbd5c8fe6800db489
Andrew Secco <asecco99@gmail.com>
2022-07-21 21:07:29 -0400

    Removed hover info for thumbnails

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,22 +1,16 @@
 import Image from 'next/image';
 import {forwardRef} from 'react';
-import React, {useState} from 'react';
 import {useRouter} from "next/router";
 
 const Thumbnail = forwardRef(({result}, ref) => {
     const BASE_URL = 'https://image.tmdb.org/t/p/original/';
-    const [hoverInfo, setHoverInfo] = useState({display: 'none'});
 
     const router = useRouter();
     const routeQuery = () => router.push({pathname: '/MovieInfo', query: result});
 
     return (
-        <div onClick={routeQuery} ref={ref} className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50' onMouseEnter={e => { setHoverInfo({display: 'block'});}} onMouseLeave={e => { setHoverInfo({display: 'none'})}}>
-            <Image className='group-hover:opacity-30' layout='responsive' src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} alt='' height={1080} width={1920}/>
-            <div style={hoverInfo} layout='responsive' className="absolute bottom-20 p-4 group-hover:font-bold text-base">
-                <p className='line-clamp-9'>{result.description || result.overview}</p>
-            </div>
-            <p style={hoverInfo} layout='responsive' className="absolute bottom-12 p-4 group-hover:font-bold text-xl text-center">{result.release_date || result.first_air_date}</p>
+        <div onClick={routeQuery} ref={ref} className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
+            <Image className='group-hover:opacity-50' layout='responsive' src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} alt='' height={1080} width={1920}/>
             <div className='p-2 text-center'>
                 <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold group-hover:text-red-400 truncate max-w-md'>{result.title || result.original_name}</h2>
             </div>
--- pages/Search.js
+++ pages/Search.js
@@ -12,6 +12,7 @@ import FlipMove from "react-flip-move";
 import {useRouter} from "next/router";
 
 function Search() {
+
     const [searchResults, setSearchResults] = useState([]);
 
     const search = async (event) => {
@@ -27,31 +28,31 @@ function Search() {
     const router = useRouter();
     const routePage = (page) => router.push(page);
 
-  return (
-    <div>
-        <header className="flex flex-col sm:flex-row mb-5 justify-between items-center h-auto">
-            <div className='flex flex-grow max-w-2xl'>
-                <div onClick={() => routePage('/')}><HeaderItem title='HOME' Icon={HomeIcon} /></div>
-                <div onClick={() => routePage('/')}><HeaderItem title='LIBRARY' Icon={BookmarkIcon} /></div>
-            </div>
-            <Image className='object-contain' src={PeriLogo} alt='Peri' width={250} height={100} />
-        </header>
-
-        <div className="flex items-center max-w-sm mx-auto mb-5">
-            <input ref={autoFocus} autoFocus type="text" onChange={search} className="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-1 focus:outline-red-400 text-black text-center" placeholder="Search..."></input>
-            <SearchIcon onClick={search} className='w-8 m-1 hover:cursor-pointer hover:text-red-400 active:text-red-500' />
-        </div>
-        
+    return (
         <div>
-            <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6">
-                {searchResults.map((movie) => movie.backdrop_path && (
-                <>
-                    <Thumbnail result={movie} />
-                </>
-                ))}
-            </FlipMove>
+            <header className="flex flex-col sm:flex-row mb-5 justify-between items-center h-auto">
+                <div className='flex flex-grow max-w-2xl'>
+                    <div onClick={() => routePage('/')}><HeaderItem title='HOME' Icon={HomeIcon} /></div>
+                    <div onClick={() => routePage('/')}><HeaderItem title='LIBRARY' Icon={BookmarkIcon} /></div>
+                </div>
+                <Image className='object-contain' src={PeriLogo} alt='Peri' width={250} height={100} />
+            </header>
+
+            <div className="flex items-center max-w-sm mx-auto mb-5">
+                <input ref={autoFocus} autoFocus type="text" onChange={search} className="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-1 focus:outline-red-400 text-black text-center" placeholder="Search..."></input>
+                <SearchIcon onClick={search} className='w-8 m-1 hover:cursor-pointer hover:text-red-400 active:text-red-500' />
+            </div>
+            
+            <div>
+                <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6">
+                    {searchResults.map((movie) => movie.backdrop_path && (
+                    <>
+                        <Thumbnail result={movie} />
+                    </>
+                    ))}
+                </FlipMove>
+            </div>
         </div>
-    </div>
   );
 }
 
