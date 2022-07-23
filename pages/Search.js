be8fd22b0a89615bce3ebe9b5d76bc104ce80b7b
Andrew Secco <asecco99@gmail.com>
2022-07-22 20:18:07 -0400

    Added upcoming movies page

--- pages/Search.js
+++ pages/Search.js
@@ -3,6 +3,7 @@ import {
     HomeIcon,
     BookmarkIcon,
     SearchIcon,
+    CalendarIcon,
 } from '@heroicons/react/outline';
 import Image from 'next/image';
 import PeriLogo from '../public/peri.png';
@@ -33,6 +34,7 @@ function Search() {
             <header className="flex flex-col sm:flex-row mb-5 justify-between items-center h-auto">
                 <div className='flex flex-grow max-w-2xl'>
                     <div onClick={() => routePage('/')}><HeaderItem title='HOME' Icon={HomeIcon} /></div>
+                    <div onClick={() => routePage('/Upcoming')}><HeaderItem title='UPCOMING' Icon={CalendarIcon} /></div>
                     <div onClick={() => routePage('/Favorites')}><HeaderItem title='FAVORITES' Icon={BookmarkIcon} /></div>
                 </div>
                 <Image className='object-contain' src={PeriLogo} alt='Peri' width={250} height={100} />
