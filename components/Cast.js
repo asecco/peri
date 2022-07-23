e1c4d98ee4aad3ea3273378736216711d18b8ce8
Andrew Secco <asecco99@gmail.com>
2022-07-23 01:36:25 -0400

    Updated cast component styles

--- components/Cast.js
+++ components/Cast.js
@@ -4,12 +4,12 @@ function Cast({member}) {
     const BASE_URL = 'https://image.tmdb.org/t/p/original/';
     return (
         <div>
-            <div className="text-center px-14 py-10 mx-auto">
+            <div className="group text-center px-14 py-10 mx-auto transition duration-200 ease-in transform cursor-pointer">
                     <div className="mb-2 sm:px-14 md:px-10 lg:px-2">
-                        <Image className="rounded-full hover:animate-pulse" layout='responsive' src={`${BASE_URL}${member.profile_path}`} title={member.name} alt='' height={192} width={176}/>
+                        <Image className="rounded-3xl group-hover:opacity-50" layout='responsive' src={`${BASE_URL}${member.profile_path}`} title={member.name} alt='' height={192} width={176}/>
                     </div>
-                    <p className="text-2xl font-bold text-red-400 truncate">{member.name}</p>
-                    <p className="text-lg font-bold truncate">{member.character}</p>
+                    <p className="text-2xl font-bold transition-all duration-100 ease-in-out text-white group-hover:text-red-400 truncate">{member.name}</p>
+                    <p className="text-md font-bold transition-all duration-100 ease-in-out text-white truncate">{member.character}</p>
             </div>
         </div>
     );
