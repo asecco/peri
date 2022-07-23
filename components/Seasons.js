a4cdf2d7d0944db20149ec20cdf46e4e77e1d74f
Andrew Secco <asecco99@gmail.com>
2022-07-23 11:45:41 -0400

    Added tv show season posters/episode lists

--- components/Seasons.js
+++ components/Seasons.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import React, {useState} from 'react';
+import {useRouter} from "next/router";
 
-function Seasons({result}) {
+function Seasons({result, id}) {
     const BASE_URL = 'https://image.tmdb.org/t/p/original/';
+    const router = useRouter();
+    const routeQuery = () => router.push({pathname: '/EpisodeList', query: {showId: id, season: result.season_number}});
+
     return (
-        <div className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
+        <div onClick={routeQuery} className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
             <Image className='group-hover:opacity-50' layout='responsive' src={`${BASE_URL}${result.poster_path}`} alt='' height={1440} width={960}/>
             <div className='p-2 text-center'>
                 <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold group-hover:text-red-400 truncate max-w-md'>{`Season ${result.season_number}`}</h2>
