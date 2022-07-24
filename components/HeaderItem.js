a66312257d2cd500f61a862c6fceb9618a8f39e3
Andrew Secco <asecco99@gmail.com>
2022-07-23 23:40:35 -0400

    Changed header and navbar text color

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,6 +1,6 @@
 function HeaderItem({Icon, title}) {
     return (
-        <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-red-400">
+        <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 text-white hover:text-red-400">
             <Icon className="h-8 mb-1 group-hover:animate-bounce"/>
             <p className="opacity-0 group-hover:opacity-100 tracking-widest">{title}</p>
         </div>
--- components/Nav.js
+++ components/Nav.js
@@ -8,7 +8,7 @@ function Nav() {
         <nav className="relative">
             <div className="flex px-10 p-2 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
                 {Object.entries(requests).map(([key, {title, url}]) => (
-                    <h2 key={key} onClick={() => router.push(`/?genre=${key}`)} className="last:pr-10 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-red-400 active:text-red-500">{title}</h2>
+                    <h2 key={key} onClick={() => router.push(`/?genre=${key}`)} className="last:pr-10 cursor-pointer transition duration-100 transform hover:scale-125 text-white hover:text-red-400 active:text-red-500">{title}</h2>
                 ))}
             </div>
             <div className="absolute top-0 right-0 bg-gradient-to-l from-[#4a5c5b] h-10 w-1/12" />
