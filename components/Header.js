3c89b8be69e859500dfe7c526dacb3a0c84a77db
Andrew Secco <asecco99@gmail.com>
2022-07-23 23:40:35 -0400

    Changed header and navbar text color

--- components/Header.js
+++ components/Header.js
@@ -28,7 +28,7 @@ function Header() {
 
             <div className="flex items-center max-w-sm mx-auto mb-5">
                 <input type="text" onClick={() => routePage('/Search')} className="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-1 focus:outline-red-400 text-black text-center ml-10 md:ml-0" placeholder="Search..."></input>
-                <SearchIcon onClick={() => routePage('/Search')} className='w-8 m-1 hover:cursor-pointer hover:text-red-400 active:text-red-500 invisible md:visible' />
+                <SearchIcon onClick={() => routePage('/Search')} className='w-8 m-1 hover:cursor-pointer text-white hover:text-red-400 active:text-red-500 invisible md:visible' />
             </div>
         </div>
     );
--- pages/Search.js
+++ pages/Search.js
@@ -44,7 +44,7 @@ function Search() {
 
             <div className="flex items-center max-w-sm mx-auto mb-5">
                 <input ref={autoFocus} autoFocus type="text" onChange={search} className="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-1 focus:outline-red-400 text-black text-center" placeholder="Search..."></input>
-                <SearchIcon onClick={search} className='w-8 m-1 hover:cursor-pointer hover:text-red-400 active:text-red-500 invisible md:visible' />
+                <SearchIcon onClick={search} className='w-8 m-1 hover:cursor-pointer text-white hover:text-red-400 active:text-red-500 invisible md:visible' />
             </div>
             
             <div>
