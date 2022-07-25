eba0e7e29a4218f667ec712a3e17874365834cd9
Andrew Secco <asecco99@gmail.com>
2022-07-25 18:46:01 -0400

    Added separate movies page with genres and pages

---
+++ pages/Movies.js
@@ -0,0 +1,66 @@
+import React, {useState, useEffect} from 'react';
+import {
+    ArrowCircleLeftIcon,
+    ArrowCircleRightIcon,
+} from '@heroicons/react/outline';
+import HeaderItem from "../components/HeaderItem";
+import Header from '../components/Header';
+import Thumbnail from '../components/Thumbnail';
+import FlipMove from 'react-flip-move';
+import requests from '../utils/requests';
+
+function Movies() {
+    const [movies, setMovies] = useState([]);
+    const [genre, setGenre] = useState('Popular');
+    const [page, setPage] = useState(1);
+    if(page < 1) {
+        setPage(1);
+    }
+    useEffect(() => {
+        const searchReq = async () => {
+            const req = await fetch(`https://api.themoviedb.org/3/${requests[genre].url}&page=${page}`).then((res) => res.json());
+            const arr = req.results;
+            arr.forEach(obj => {
+                obj.media_type = 'movie';
+            });
+            setMovies(arr);
+        }
+        searchReq();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }, [page, genre]);
+
+    return (
+        <div>
+            <Header />
+            <nav className="relative">
+                <div className="flex px-10 p-2 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
+                    {Object.entries(requests).map(([key, {title}]) => (
+                        <h2 key={key} onClick={() => setGenre(key)} className="last:pr-10 cursor-pointer transition duration-100 transform hover:scale-125 text-white hover:text-red-400 active:text-red-500">{title}</h2>
+                    ))}
+                </div>
+                <div className="absolute top-0 right-0 bg-gradient-to-l from-[#202F3B] h-10 w-1/12" />
+            </nav>
+
+            <p className='font-bold text-white text-3xl md:text-4xl lg:text-5xl mx-7 my-4'>{genre}</p>
+            <div>
+                <FlipMove className="px-5 my-10 sm:grid md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6">
+                    {movies.map((movie) => (
+                    <>
+                        <Thumbnail result={movie} />
+                    </>
+                    ))}
+                </FlipMove>
+            </div>
+
+            <div className='flex flex-row sm:flex-row justify-between items-center h-auto'>
+                <div onClick={() => setPage(page - 1)}><HeaderItem title='Previous' Icon={ArrowCircleLeftIcon} /></div>
+                <div onClick={() => setPage(page + 1)}><HeaderItem title='Next' Icon={ArrowCircleRightIcon} /></div>
+            </div>
+        </div>
+    );
+}
+
+export default Movies;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Header from '../components/Header';
-import NavBar from '../components/NavBar';
 import HomeScreen from '../components/HomeScreen';
 import requests from '../utils/requests';
 import SearchBar from '../components/SearchBar';
@@ -15,7 +14,6 @@ export default function Home({results}) {
       </Head>
       <Header />
       <SearchBar />
-      <NavBar />
       <HomeScreen results={results} />
     </div>
   );
@@ -23,9 +21,8 @@ export default function Home({results}) {
 
 const trending = `trending/all/week?api_key=${process.env.API_KEY}`;
 
-export async function getServerSideProps(context) {
-  const genre = context.query.genre
-  const req = await fetch(`https://api.themoviedb.org/3/${requests[genre]?.url || trending}`).then((res) => res.json())
+export async function getServerSideProps() {
+  const req = await fetch(`https://api.themoviedb.org/3/${trending}`).then((res) => res.json())
 
   return {
     props: {
