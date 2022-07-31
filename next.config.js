fa0e9d43ea97b883932ffb8b4db0a817afe55887
Andrew Secco <asecco99@gmail.com>
2022-07-31 02:40:10 -0400

    Changed config setting due to optimization warning

--- next.config.js
+++ next.config.js
@@ -3,8 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['image.tmdb.org']
-  }
+    domains: ['image.tmdb.org'],
+  },
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
 }
 
 module.exports = nextConfig
