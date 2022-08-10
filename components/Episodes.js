9cec193c4f68b5f270e8673e158e17aca514acfb
Andrew Secco <asecco99@gmail.com>
2022-08-10 14:12:22 -0400

    Responsive styling for new episodes layout

--- components/Episodes.js
+++ components/Episodes.js
@@ -4,16 +4,16 @@ import { StarIcon } from '@heroicons/react/outline';
 
 function Episodes({result}) {
     return (
-        <div className="grid grid-cols-2 w-full p-2 hover:bg-gray-600">
+        <div className="grid grid-cols-1 lg:grid-cols-2 w-full p-2 hover:bg-gray-600">
             <Image layout="responsive" src={`${BASE_URL}${result.still_path}`} alt='' height={1080} width={1920}/>
             <div className="p-6">
-                <div className="text-red-400 font-bold text-xl mb-2">{`${result.episode_number}. ${result.name}`}</div>
-                <div className="flex items-center space-x-10 m-2 text-sm md:text-base text-white">
+                <div className="text-red-400 font-bold text-lg lg:text-xl">{`${result.episode_number}. ${result.name}`}</div>
+                <div className="flex items-center text-center space-x-2 md:space-x-4 lg:space-x-10 m-2 text-sm md:text-base text-white">
                     <p>{`${result.runtime} mins`}</p>
                     <p>{result.air_date}</p>
-                    <StarIcon className="h-4 my-4 md:h-6 lg:mx-2 lg:my-0 text-yellow-400 fill-yellow-400" />{Math.round(result.vote_average * 10) / 10}/10
+                    <StarIcon className="h-4 my-4 md:h-5 lg:h-6 lg:mx-2 lg:my-0 text-yellow-400 fill-yellow-400" />{Math.round(result.vote_average * 10) / 10}/10
                 </div>
-                <p className="text-white text-lg line-clamp-8">{result.overview}</p>
+                <p className="text-white md:text-lg line-clamp-8 md:line-clamp-6 lg:line-clamp-8">{result.overview}</p>
             </div>
         </div>
     );
