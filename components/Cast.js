bfd19704fbb2d4d039c0b6488daa4dd3d32fdfa7
Andrew Secco <asecco99@gmail.com>
2022-08-10 23:39:27 -0400

    Removed cast comp name truncating

--- components/Cast.js
+++ components/Cast.js
@@ -12,8 +12,8 @@ function Cast({member}) {
                 <div className="mb-2 sm:px-14 md:px-10 lg:px-2">
                     <Image className="rounded-3xl group-hover:opacity-50" layout='responsive' src={`${BASE_URL}${member.profile_path}`} title={member.name} alt='' height={240} width={160}/>
                 </div>
-                <p className="text-2xl font-bold transition-all duration-100 ease-in-out text-white group-hover:text-red-400 md:truncate">{member.name}</p>
-                <p className="my-2 text-md font-bold transition-all duration-100 ease-in-out text-white md:truncate">{member.character}</p>
+                <p className="text-2xl font-bold transition-all duration-100 ease-in-out text-white group-hover:text-red-400 md:line-clamp-2">{member.name}</p>
+                <p className="my-2 text-md font-bold transition-all duration-100 ease-in-out text-white md:line-clamp-2">{member.character}</p>
             </div>
         </div>
     );
