03850af26419cabb0d0b83f2e7e66327bccb10b8
Andrew Secco <asecco99@gmail.com>
2022-08-13 16:17:56 -0400

    Fixed search bar centering on mobile

--- pages/Search.js
+++ pages/Search.js
@@ -23,8 +23,8 @@ function Search() {
     return (
         <div>
             <Header />
-            <div className="flex items-center max-w-sm mx-auto mb-5">
-                <input ref={autoFocus} autoFocus type="text" onChange={search} className="h-14 w-96 pr-8 pl-5 rounded-full z-0 focus:shadow focus:outline-1 focus:outline-red-400 text-black text-center" placeholder="Search..."></input>
+            <div className="flex items-center ml-12 md:ml-auto mx-auto max-w-sm mb-5">
+                <input ref={autoFocus} autoFocus type="text" onChange={search} className="h-14 w-96 rounded-full focus:shadow focus:outline-1 focus:outline-red-400 text-black text-center" placeholder="Search..."></input>
                 <SearchIcon onClick={search} className='w-8 m-1 hover:cursor-pointer text-white hover:text-red-400 active:text-red-500 invisible md:visible' />
             </div>
 
