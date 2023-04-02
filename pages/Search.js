88422510be2ab15bd6734c7904162975acc83c13
Andrew Secco <asecco99@gmail.com>
2023-04-02 00:29:57 -0400

    Improved search function, no longer duplicates/random results

--- pages/Search.js
+++ pages/Search.js
@@ -7,24 +7,29 @@ import FlipMove from "react-flip-move";
 import { API_KEY, API_URL } from '../utils/constants';
 
 function Search() {
+    const [searchQuery, setSearchQuery] = useState("");
     const [searchResults, setSearchResults] = useState([]);
+
+    const handleInputChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
     const search = async (event) => {
-        const searchQuery = event.target.value;
-        if (searchQuery) {
-            const searchReq = await fetch(`${API_URL}search/multi?api_key=${API_KEY}&language=en-US&query=${searchQuery}&include_adult=false`).then((res) => res.json());
-            setSearchResults(searchReq.results);
-        }
-    }
-    const autoFocus = useCallback(el => el ? el.focus() : null, []);
+        event.preventDefault();
+        const searchReq = await fetch(`${API_URL}search/multi?api_key=${API_KEY}&language=en-US&query=${searchQuery}&include_adult=false`).then((res) => res.json());
+        setSearchResults(searchReq.results);
+    };
+    
+    const autoFocus = useCallback(e => e ? e.focus() : null, []);
 
     return (
         <div>
             <Head><title>Peri</title></Head>
             <Header />
-            <div className="flex items-center ml-12 md:ml-auto mx-auto max-w-sm mb-5">
-                <input ref={autoFocus} autoFocus type="text" onChange={search} className="h-14 w-96 rounded-full focus:shadow focus:outline-1 focus:outline-red-400 text-black text-center" placeholder="Search..."></input>
+            <form onSubmit={search} className="flex items-center ml-12 md:ml-auto mx-auto max-w-sm mb-5">
+                <input ref={autoFocus} autoFocus type="text" value={searchQuery} onChange={handleInputChange} className="h-14 w-96 rounded-full focus:shadow focus:outline-1 focus:outline-red-400 text-black text-center" placeholder="Search..."></input>
                 <SearchIcon onClick={search} className='w-8 m-1 hover:cursor-pointer text-white hover:text-red-400 active:text-red-500 invisible md:visible' />
-            </div>
+            </form>
 
             <div>
                 <FlipMove className="px-5 my-10 sm:grid md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 3xl:grid-cols-7">
