e5b6578cec12f32675e43a11ed41ea4ae9a34531
Andrew Secco <asecco99@gmail.com>
2023-04-05 14:00:55 -0400

    Updated episodelist with ssr

--- next.config.js
+++ next.config.js
@@ -1,15 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['image.tmdb.org'],
-  },
-  experimental: {
+    reactStrictMode: true,
+    swcMinify: true,
     images: {
-      unoptimized: true,
+        domains: ['image.tmdb.org'],
     },
-  },
-}
+    experimental: {
+        images: {
+            unoptimized: true,
+        },
+    },
+    async rewrites() {
+        return [
+        {
+            source: '/tv/:id/season/:season_number',
+            destination: '/EpisodeList',
+        },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/EpisodeList.js
+++ pages/EpisodeList.js
@@ -1,48 +1,45 @@
-import React, { useState, useEffect } from 'react';
 import Image from "next/image";
 import Header from '../components/Header';
 import Episodes from '../components/Episodes';
 import Head from 'next/head';
-import { useRouter } from "next/router";
 import { BASE_URL, API_KEY, API_URL } from '../utils/constants';
 
-function EpisodeList() {
-    const router = useRouter();
-    const showId = router.query.showId;
-    const seasonNum = router.query.season;
-    const overview = router.query.overview;
-    const poster_path = router.query.poster;
-    const tv_title = router.query.title;
-    const [episodes, setEpisodes] = useState([]);
-    useEffect(() => {
-        const searchReq = async () => {
-            const episodeReq = await fetch(`${API_URL}tv/${showId}/season/${seasonNum}?api_key=${API_KEY}&language=en-US`).then((res) => res.json());
-            setEpisodes(episodeReq.episodes);
-        }
-        searchReq();
-    }, []);
-
+function EpisodeList({ season, episodes, title, showId }) {
     return (
         <div>
-            <Head><title>{tv_title}</title></Head>
+            <Head><title>{title}</title></Head>
             <Header />
             <div className="mx-auto px-20 flex flex-col-reverse gap-10 object-bottom md:flex-row">
                 <div className="flex flex-col gap-4 md:w-5/12 lg:w-6/12 xl:w-8/12 2xl:w-10/12">
-                    <h1 className="font-bold text-3xl md:text-5xl lg:text-7xl text-center text-red-400">{`Season ${seasonNum}`}</h1>
-                    <p className="text-center md:text-left text-base md:text-xl lg:text-2xl text-white line-clamp-14 mb-6 md:mb-0">{overview}</p>
+                    <h1 className="font-bold text-3xl md:text-5xl lg:text-7xl text-center text-red-400">{`Season ${season.season_number}`}</h1>
+                    <p className="text-center md:text-left text-base md:text-xl lg:text-2xl text-white line-clamp-14 mb-6 md:mb-0">{season.overview}</p>
                     {episodes?.map((episode) => episode.still_path && (
                     <>
                         <Episodes result={episode} />
                     </>
                     ))}
                 </div>
-
                 <div className="w-8/12 md:w-4/12 lg:w-3/12 mx-10 md:mx-28 lg:mx-14">
-                    <Image layout="responsive" src={`${BASE_URL}${poster_path}`} alt='' height={960} width={640}/>
+                    <Image layout="responsive" src={`${BASE_URL}${season.poster_path}`} alt='' height={960} width={640}/>
                 </div>
             </div>
         </div>
     );
+}
+
+export async function getServerSideProps(context) {
+    const { result, showId, title } = context.query;
+    const season = JSON.parse(result);
+    const episode = await fetch(`${API_URL}tv/${showId}/season/${season.season_number}?api_key=${API_KEY}&language=en-US`).then((res) => res.json());
+
+    return {
+        props: {
+            season: season,
+            episodes: episode.episodes,
+            title: title,
+            showId: showId,
+        },
+    }
 }
 
 export default EpisodeList;
