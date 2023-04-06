d8dfcba34ea47f0829a3ea9eac0f4a934adb644d
Andrew Secco <asecco99@gmail.com>
2023-04-06 02:16:57 -0400

    Added footer page number btns for pagination

--- components/FooterItem.js
+++ components/FooterItem.js
@@ -1,7 +1,7 @@
 function FooterItem({Icon, title}) {
     return (
         <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 text-white hover:text-red-400">
-            <Icon className="h-12 mb-1"/>
+            <Icon className="h-12"/>
             <p className="opacity-0 group-hover:opacity-100 tracking-widest">{title}</p>
         </div>
     );
--- pages/Movies.js
+++ pages/Movies.js
@@ -11,6 +11,7 @@ function Movies() {
     const [movies, setMovies] = useState([]);
     const [genre, setGenre] = useState('Popular');
     const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
     if(page < 1) setPage(1);
     useEffect(() => {
         document.title = `Movies | ${genre}`;
@@ -21,6 +22,7 @@ function Movies() {
                 obj.media_type = 'movie';
             });
             setMovies(arr);
+            setTotalPages(req.total_pages);
         }
         searchReq();
         window.scrollTo({
@@ -28,6 +30,40 @@ function Movies() {
             behavior: 'smooth'
         });
     }, [page, genre]);
+
+    useEffect(() => {
+        setPage(1);
+    }, [genre]);
+
+    const handleClick = (pageNumber) => {
+        setPage(pageNumber);
+    };
+
+    const pagesToShow = 5;
+    const getPageNumbers = () => {
+        const pageNumbers = [];
+        // If there is only one page or no pages, return an empty array
+        if (totalPages <= 1) {
+            return pageNumbers;
+        }
+        // Calculate the first and last page numbers to show
+        let firstPage = Math.max(1, page - Math.floor(pagesToShow / 2));
+        let lastPage = Math.min(totalPages, firstPage + pagesToShow - 1);
+        // Adjust the first and last page numbers if necessary to show exactly `pagesToShow` pages
+        if (lastPage - firstPage + 1 < pagesToShow) {
+            if (firstPage === 1) {
+                lastPage = Math.min(totalPages, pagesToShow);
+            } else {
+                firstPage = Math.max(1, lastPage - pagesToShow + 1);
+            }
+        }
+        // Add the page numbers to the array
+        for (let i = firstPage; i <= lastPage; i++) {
+            pageNumbers.push(i);
+        }
+        return pageNumbers;
+    };
+    const pageNumbers = getPageNumbers();
 
     return (
         <div>
@@ -54,6 +90,11 @@ function Movies() {
 
             <div className='flex flex-row sm:flex-row justify-between items-center h-auto'>
                 <div onClick={() => setPage(page - 1)}><FooterItem title='Previous' Icon={ArrowCircleLeftIcon} /></div>
+                    <div className="flex justify-center space-x-4 mb-6">
+                        {pageNumbers.map((pageNumber) => (
+                            <button key={pageNumber} onClick={() => handleClick(pageNumber)} className={`${pageNumber === page ? 'bg-red-500 text-white' : 'bg-white text-black hover:opacity-75'} text-xl px-4 py-2 rounded-full font-bold`}>{pageNumber}</button>
+                        ))}
+                    </div>
                 <div onClick={() => setPage(page + 1)}><FooterItem title='Next' Icon={ArrowCircleRightIcon} /></div>
             </div>
         </div>
