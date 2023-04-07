6cafdb4c19e4c0a11ae9b054de710ccf23ba6d1d
Andrew Secco <asecco99@gmail.com>
2023-04-07 00:21:41 -0400

    Fixed NowPlayingBanner not expanding for larger screens

--- components/NowPlayingBanner.js
+++ components/NowPlayingBanner.js
@@ -25,11 +25,11 @@ function NowPlayingBanner({ nowPlaying }) {
                 {nowPlaying.slice(0, 3).map((movie) => (
                     <div onClick={() => routeQuery(movie)} className='w-full relative h-96 overflow-hidden px-8 hover:cursor-pointer' key={movie.id}>
                         <div className="absolute w-full h-full opacity-50 top-0 left-0 z-10 bg-gradient-to-r from-primary via-primary"></div>
-                        <div className="absolute z-20 flex flex-col items-left w-1/2 justify-center h-full px-10">
+                        <div className="absolute z-20 flex flex-col items-left w-1/2 justify-center h-full px-10 lg:w-1/3">
                             <div className="text-red-400 font-bold text-4xl lg:text-5xl text-center">{movie.title || movie.original_title}</div>
                             <div className="text-white text-lg lg:text-xl mt-4 font-medium md:line-clamp-4 lg:line-clamp-6">{movie.overview}</div>
                         </div>
-                        <Image src={`${BASE_URL}${movie.backdrop_path || movie.poster_path}`}  alt='' height={1080} width={1920}  />
+                        <Image className="object-center object-cover" layout="fill" objectFit="cover" src={`${BASE_URL}${movie.backdrop_path || movie.poster_path}`} alt='' />
                     </div>
                 ))}
             </Slider>
