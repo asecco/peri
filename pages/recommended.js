23d93113015c1171cc66d37e614faa0e1c2fb6fc
Andrew Secco <asecco99@gmail.com>
2023-05-23 04:33:09 -0400

    Sorted recommended by # of votes

--- pages/recommended.js
+++ pages/recommended.js
@@ -31,6 +31,9 @@ function Recommended() {
             const filteredArr = recArr.filter((obj) => { //Removes movies that are already in favorites
                 return !favoriteIds.includes(obj.id);
             });
+            filteredArr.sort((a, b) => { //Sorts by vote count
+                return b.vote_count - a.vote_count;
+            });
             setRecRes(filteredArr.splice(0, 20));
         }
         searchReq();
