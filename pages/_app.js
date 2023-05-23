acb2d61ddcdefdf32c86f90fbc249f6b5dcacd58
Andrew Secco <asecco99@gmail.com>
2023-05-23 10:02:35 -0400

    Added vercel/analytics to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css';
 import '../styles/modal.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp
