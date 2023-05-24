acbdf7d09652bf3695ce1be072b4f9637a55719e
Andrew Secco <asecco99@gmail.com>
2023-05-24 17:36:05 -0400

    Updated cast panel styling

--- components/Cast.js
+++ components/Cast.js
@@ -5,7 +5,7 @@ function Cast({ member }) {
     return (
         <div>
             <a href={`/cast?id=${member.id}`} rel="noopener noreferrer">
-                <div className="group text-center px-4 md:px-6 xl:px-14 2xl:px-12 py-6 mx-auto transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 cursor-pointer">
+                <div className="group text-center px-2 md:px-0 py-4 mx-auto transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 cursor-pointer">
                     <div className="mb-2 sm:px-14 md:px-10 lg:px-2">
                         <Image className="rounded-3xl group-hover:opacity-50" layout='responsive' src={`${BASE_URL}${member.profile_path}`} title={member.name} alt='' height={240} width={160}/>
                     </div>
