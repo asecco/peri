6f87e8bb2e0b545976f3f01fbc8cedc11c12676e
Andrew Secco <asecco99@gmail.com>
2023-05-25 15:27:34 -0400

    Added tooltips for hover of movie title

--- components/Recommend.js
+++ components/Recommend.js
@@ -6,7 +6,7 @@ function Recommend({ result }) {
         <div className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
             <a href={`/info?type=${result.media_type || 'movie'}&id=${result.id}`} rel="noopener noreferrer">
                 <Image className='group-hover:opacity-50' layout='responsive' src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} alt='' height={1080} width={1920}/>
-                <div className='p-2 text-center'>
+                <div className='p-2 text-center' title={result.title || result.original_name}>
                     <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold group-hover:text-red-400 truncate max-w-md'>{result.title || result.original_name}</h2>
                 </div>
             </a>
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -7,7 +7,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
         <div ref={ref} className='p-2 mx-10 my-2 md:mx-4 lg:mx-8 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
             <a href={`/info?type=${result.media_type}&id=${result.id}`} rel="noopener noreferrer">
                 <Image className='group-hover:opacity-50' layout='responsive' src={`${BASE_URL}${result.poster_path}`} alt='' height={1440} width={960}/>
-                <div className='p-2 text-center'>
+                <div className='p-2 text-center' title={result.title || result.original_name}>
                     <h2 className='mt-1 text-xl lg:text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold group-hover:text-red-400 truncate max-w-md'>{result.title || result.original_name}</h2>
                 </div>
             </a>
