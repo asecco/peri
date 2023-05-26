b8f16fb3529a977684240bed081aa6ce826a41e6
Andrew Secco <asecco99@gmail.com>
2023-05-26 19:12:36 -0400

    Fixed styling on search and adjusted autocomplete

--- pages/search.js
+++ pages/search.js
@@ -53,18 +53,20 @@ function Search() {
             <Head><title>Peri</title></Head>
             <Header />
             <form onSubmit={search} className="flex items-center mx-auto max-w-xl mb-5 relative">
-                <input ref={(ref) => searchInputRef.current = ref} type="text" value={searchQuery} onChange={handleInputChange} onClick={clearSearchResults} className="h-16 w-full rounded-md focus:shadow focus:outline-1 focus:outline-red-400 text-black text-center text-2xl lg:text-3xl" placeholder="Search..."></input>
-                <SearchIcon onClick={search} className='absolute right-1 w-8 hover:cursor-pointer text-primary hover:text-red-400 active:text-red-500'/>
-            </form>
-            {autoCompleteResults.length > 0 && searchResults.length === 0 && (
-                <div className="max-w-xl mx-auto">
-                    {autoCompleteResults.map((result) => (
-                        <a href={`/info?type=${result.media_type}&id=${result.id}`} rel="noopener noreferrer">
-                            <div key={result.id} className="text-white bg-gray-800 text-center rounded-md shadow-md p-2 mb-2 cursor-pointer">{result.title || result.name} • {result.media_type} • {result.release_date || result.first_air_date}</div>
-                        </a>
-                    ))}
+                <div className="relative w-full">
+                    <input ref={(ref) => searchInputRef.current = ref} type="text" value={searchQuery} onChange={handleInputChange} onClick={clearSearchResults} className="h-16 w-full rounded-md focus:shadow focus:outline-1 focus:outline-red-400 text-black text-center text-2xl lg:text-3xl" placeholder="Search..."></input>
+                    {autoCompleteResults.length > 0 && searchResults.length === 0 && (
+                        <div className="absolute z-10 w-full mt-1">
+                            {autoCompleteResults.map((result) => (
+                                <a href={`/info?type=${result.media_type}&id=${result.id}`} rel="noopener noreferrer">
+                                    <div key={result.id} className="text-white bg-gray-800 text-center hover:text-red-400 rounded-md shadow-md p-2 mb-2 cursor-pointer">{result.title || result.name} • {result.media_type} • {result.release_date ? result.release_date.substring(0, 4) : (result.first_air_date ? result.first_air_date.substring(0, 4) : '')}</div>
+                                </a>
+                            ))}
+                        </div>
+                    )}
                 </div>
-            )}
+                <SearchIcon onClick={search} className='absolute right-2 top-1/2 transform -translate-y-1/2 w-8 hover:cursor-pointer text-primary hover:text-red-400 active:text-red-500'/>
+            </form>
             {searchResults.length > 0 && <Results results={searchResults} />}
         </div>
   );
