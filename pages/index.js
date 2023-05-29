de2ff7bacfdc7ccfe752dcdf87acbfea4cbae796
Andrew Secco <asecco99@gmail.com>
2023-05-28 21:01:25 -0400

    Refactored index and removed redundant homescreen component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Header from '../components/Header';
-import HomeScreen from '../components/HomeScreen';
+import NowPlayingBanner from "../components/NowPlayingBanner";
+import Results from "../components/Results";
 import { API_KEY, API_URL } from '../utils/constants';
 
 export default function Home({ results, nowPlaying }) {
@@ -8,18 +9,19 @@ export default function Home({ results, nowPlaying }) {
 		<div>
 			<Head>
 				<title>Peri</title>
-				<meta name="description" content="Movie/TV library for finding the perfect watch" />
+				<meta name="description" content="Movie/TV library for finding the perfect watch for the night" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Header />
-			<HomeScreen results={results} nowPlaying={nowPlaying}  />
+			<NowPlayingBanner nowPlaying={nowPlaying} />
+            <p className='font-bold text-white text-4xl lg:text-5xl mx-8 xl:mx-10 mt-14 text-center md:text-left'>Trending</p>
+            <Results results={results} />
 		</div>
   	);
 }
 
-const trending = `trending/all/week?api_key=${process.env.NEXT_PUBLIC_API_KEY}`;
-
 export async function getServerSideProps() {
+	const trending = `trending/all/week?api_key=${process.env.NEXT_PUBLIC_API_KEY}`;
 	const req = await fetch(`https://api.themoviedb.org/3/${trending}`).then((res) => res.json())
 	const nowPlaying = await fetch(`${API_URL}movie/now_playing?api_key=${API_KEY}&language=en-US`).then((res) => res.json());
 
