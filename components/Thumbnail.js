7f3db5c9fc8efce31cbfea49539216addbe6febd
Andrew Secco <asecco99@gmail.com>
2023-05-30 13:16:43 -0400

    Updated font weight on thumbnail

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -8,7 +8,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
             <a href={`/info?type=${result.media_type}&id=${result.id}`} rel="noopener noreferrer">
                 <Image priority={true} className='group-hover:opacity-50' layout='responsive' src={`${BASE_URL}${result.poster_path}`} alt='' height={1440} width={960}/>
                 <div className='p-2 text-center' title={result.title || result.original_name}>
-                    <h2 className='mt-1 text-xl lg:text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold group-hover:text-red-400 line-clamp-2 max-w-md'>{result.title || result.original_name}</h2>
+                    <h2 className='mt-1 text-xl lg:text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-medium group-hover:text-red-400 line-clamp-2 max-w-md'>{result.title || result.original_name}</h2>
                 </div>
             </a>
         </div>
