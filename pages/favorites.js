644276bed1319efb89c9e6c2ee061ca3dac85ffb
Andrew Secco <asecco99@gmail.com>
2023-06-01 14:29:49 -0400

    Added infinite scrolling for the favorites page

--- pages/favorites.js
+++ pages/favorites.js
@@ -8,26 +8,53 @@ import { API_KEY, API_URL } from '../utils/constants';
 function Favorites() {
     const [favRes, setFavRes] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
+
     useEffect(() => {
-        const searchReq = async () => {
-            const favArr = [];
+        const fetchFavorites = async () => {
             const favorites = JSON.parse(localStorage.getItem('favorites'));
-            for(let i in favorites) {
-                const favReq = await fetch(`${API_URL}${favorites[i].type}/${favorites[i].id}?api_key=${API_KEY}&language=en-US`).then((res) => res.json());
-                favReq.media_type = favorites[i].type;
+            const startIndex = (page - 1) * 10;
+            const endIndex = startIndex + 10;
+            const favSlice = favorites.slice(startIndex, endIndex);
+
+            if (startIndex >= favorites.length) {
+                setHasMore(false);
+                return;
+            }
+
+            const favArr = favRes.slice();
+            for (let i in favSlice) {
+                const favReq = await fetch(`${API_URL}${favSlice[i].type}/${favSlice[i].id}?api_key=${API_KEY}&language=en-US`).then((res) => res.json());
+                favReq.media_type = favSlice[i].type;
                 favArr.push(favReq);
             }
             setFavRes(favArr);
             setIsLoaded(true);
+        };
+        fetchFavorites();
+    }, [page]);
+
+    const handleScroll = () => {
+        if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+            if (hasMore) {
+                setPage((prevPage) => prevPage + 1);
+            }
         }
-        searchReq();
-    }, []);
-    
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [page]);
+
     return (
         <div>
             <Head><title>{`Favorites`}</title></Head>
             <Header />
-            {isLoaded &&<LocalStorageMessage results={favRes} id={'favorites'} />}
+            {isLoaded && <LocalStorageMessage results={favRes} id={'favorites'} />}
             <Results results={favRes} />
         </div>
     );
