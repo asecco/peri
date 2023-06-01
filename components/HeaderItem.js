a8f1c369defb6f7b2e6b418a0b7e59077c93b0c0
Andrew Secco <asecco99@gmail.com>
2023-06-01 01:48:26 -0400

    Added underline effect to header icons

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,8 +1,8 @@
 function HeaderItem({Icon, title}) {
     return (
-        <div className="flex flex-col items-center cursor-pointer group w-14 sm:w-20 md:w-24 lg:w-28 xl:w-32 2xl:w-36 3xl:w-40 text-white hover:text-red-400">
+        <div className="flex flex-col items-center cursor-pointer group ml-2 w-14 sm:w-20 md:w-24 lg:w-28 xl:w-32 2xl:w-36 3xl:w-40 text-white hover:text-red-400 link link-underline link-underline-black">
             <Icon className="h-12 group-hover:animate-bounce"/>
-            <p className="opacity-0 group-hover:opacity-100 tracking-wide text-base md:text-xl">{title}</p>
+            <p className="opacity-0 group-hover:opacity-100 tracking-wide text-base md:text-xl mb-2">{title}</p>
         </div>
     );
 }
