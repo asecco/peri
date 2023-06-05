9e828d8582e2f1cef2e7f812b4f1f2ec193bbb71
Andrew Secco <asecco99@gmail.com>
2023-06-04 21:01:06 -0400

    Fixed nowplaying banner positioning

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import '../styles/modal.css';
 import { Analytics } from '@vercel/analytics/react';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
     const router = useRouter();
