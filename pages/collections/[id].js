c546410ff10ebc00ab00f467116545ffbfd0b711
Andrew Secco <asecco99@gmail.com>
2023-06-07 20:31:22 -0400

    Added share button to id page

--- pages/collections/[id].js
+++ pages/collections/[id].js
@@ -7,11 +7,21 @@ import { alertParams } from '../../utils/notifications';
 
 export default function Collection({ collection }) {
     const url = `peri.vercel.app/collections/${collection?.id}`
+    const shareText = `Check out this collection: ${collection?.title}`
 
     const copyToClipboard = () => {
         navigator.clipboard.writeText(url);
         toast.success('Copied!', alertParams);
     };
+
+    const handleShare = () => {
+        if (navigator.share) {
+            navigator.share({ url, shareText });
+        } else {
+            console.log('Web Share API not supported');
+        }
+    };
+      
 
     return (
         <div>
@@ -22,6 +32,10 @@ export default function Collection({ collection }) {
                 <button onClick={copyToClipboard} title="Click to Copy" className="bg-white text-black text-lg md:text-xl italic rounded-md border-b-2 border-red-400 hover:bg-red-400 hover:text-white shadow-md py-2 px-8 inline-flex items-center">
                     <span className="mr-2">{url}</span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentcolor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
+                </button>
+                <button onClick={handleShare} title="Share" className="bg-white text-black text-lg md:text-xl rounded-md border-b-2 border-red-400 hover:bg-red-400 hover:text-white shadow-md py-2 px-8 inline-flex items-center m-2">
+                    <span className='mr-2'>Share</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line><line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line></svg>
                 </button>
             </div>
             <div>
