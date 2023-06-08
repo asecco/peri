9e3829c664f63da785c8c0918c46ba470cd8b371
Andrew Secco <asecco99@gmail.com>
2023-06-07 21:37:09 -0400

    Added next loading bar for slow pages

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import '../styles/modal.css';
 import { Analytics } from '@vercel/analytics/react';
 import { useRouter } from 'next/router';
+import NextProgress from "next-progress";
 
 function MyApp({ Component, pageProps }) {
     const router = useRouter();
@@ -9,8 +10,9 @@ function MyApp({ Component, pageProps }) {
         <>
             <Component {...pageProps} key={router.asPath} />
             <Analytics />
+            <NextProgress color={'#F87171'} height={'4px'} />
         </>
     );
 }
 
-export default MyApp
+export default MyApp;
