1411668d5bd657705f64e389373fd5be949aa598
Andrew Secco <asecco99@gmail.com>
2023-06-08 20:38:25 -0400

    Updated header icon sizes and margins

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,7 +1,7 @@
 function HeaderItem({Icon, title}) {
     return (
         <div className="flex flex-col items-center cursor-pointer group -ml-1 md:ml-2 w-16 sm:w-20 md:w-24 lg:w-28 xl:w-32 2xl:w-36 3xl:w-40 text-white hover:text-red-400 link link-underline link-underline-black">
-            <Icon className="h-12 group-hover:animate-bounce"/>
+            <Icon className="h-12 3xl:h-18 group-hover:animate-bounce"/>
             <p className="opacity-0 group-hover:opacity-100 tracking-wide text-base md:text-xl mb-2">{title}</p>
         </div>
     );
