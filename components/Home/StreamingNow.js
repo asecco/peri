1cb8d756bb57fc74cca578685919cb7bff88f217
Andrew Secco <asecco99@gmail.com>
2023-06-14 16:16:35 -0400

    Updated margin for streamingnow on mobile

--- components/Home/StreamingNow.js
+++ components/Home/StreamingNow.js
@@ -12,13 +12,13 @@ function StreamingNow({ tv }) {
             </div>
             <div className="grid grid-cols-2 ml-2 md:ml-4 md:grid-cols-3 lg:grid-cols-6 gap-6 my-10">
                 {tv?.slice(0, 6)?.map((show, index) => (
-                    <div key={show?.id} className="grid grid-cols-2 md:flex items-center ml-6">
+                    <div key={show?.id} className="grid grid-cols-2 md:flex items-center ml-10 md:ml-6">
                         <div className='flex-shrink-0 hover:opacity-50 transition duration-200 ease-in transform sm:hover:scale-105'>
                             <Link href={`/info/tv/${show?.id}`}>
                                 <Image className="rounded-lg" src={`${BASE_URL}${show?.poster_path}`} alt='' width={160} height={240}/>
                             </Link>
                         </div>
-                        <p className="text-white font-black mx-auto md:mx-2 text-5xl md:text-6xl lg:text-7xl 3xl:text-9xl">{index + 1}</p>
+                        <p className="text-white font-black mx-4 md:mx-2 text-5xl md:text-6xl lg:text-7xl 3xl:text-9xl">{index + 1}</p>
                     </div>
                 ))}
             </div>
