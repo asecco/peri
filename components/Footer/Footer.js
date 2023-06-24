246085161241fae9dd6740f1595f74ad4ecf2404
Andrew Secco <asecco99@gmail.com>
2023-06-24 01:28:58 -0400

    Added alert and prevent to increasing page when on the last page

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,10 +2,16 @@ import { ArrowCircleLeftIcon, ArrowCircleRightIcon } from '@heroicons/react/outl
 import FooterItem from "./FooterItem";
 import PaginationFooter from './PaginationFooter';
 import { useRouter } from "next/router";
+import { toast } from 'react-toastify';
+import { alertParams } from "../../utils/helper";
 
 function Footer({ type, genre, page, totalPages, voteAverage, minYear, maxYear, sortBy }) {
     const router = useRouter();
     const pageRoute = (pageNumber) => {
+        if (pageNumber > totalPages) {
+            toast.error('No more pages', alertParams);
+            return;
+        }
         const newPage = Math.max(pageNumber, 1);
         router.push({ pathname: `/${type}/${genre}/${newPage}`, query: { voteAverage: voteAverage, minYear: minYear, maxYear: maxYear, sortBy: sortBy }});
     }
