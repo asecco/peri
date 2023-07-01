7dd6492a5b9e6bf9626c1fd5ba2107cf4457a5bd
Andrew Secco <asecco99@gmail.com>
2023-07-01 14:24:33 -0400

    Updated desktop header layout

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,4 +1,4 @@
-import { HomeIcon, MapIcon, CalendarIcon, VideoCameraIcon, FilmIcon, SearchIcon, HeartIcon, ThumbUpIcon, CollectionIcon, MenuIcon } from '@heroicons/react/outline';
+import { HomeIcon, CalendarIcon, VideoCameraIcon, FilmIcon, SearchIcon, HeartIcon, ThumbUpIcon, CollectionIcon, MenuIcon } from '@heroicons/react/outline';
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -7,11 +7,7 @@ import HeaderItem from './HeaderItem';
 import About from './About';
 
 function Header() {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const handleDropdownOpen = () => setIsDropdownOpen(true);
-    const handleDropdownClose = () => setIsDropdownOpen(false);
     const [mobileMenu, setMobileMenu] = useState(false);
-
     const [isMobile, setIsMobile] = useState(false);
     useEffect(() => {
         window.innerWidth <= 767 ? setIsMobile(true) : setIsMobile(false);
@@ -21,51 +17,44 @@ function Header() {
         <div>
             <About />
             {!isMobile ? (
-                <header className="flex flex-col sm:flex-row mb-5 justify-between items-center h-auto mt-2">
+                <header className="flex flex-col sm:flex-row mb-2 justify-between items-center h-auto mt-2">
                     <div className='flex flex-grow max-w-2xl mt-2 md:mt-0'>
                         <Link href={`/`}><HeaderItem title='HOME' Icon={HomeIcon} /></Link>
                         <Link href={`/search`}><HeaderItem title='SEARCH' Icon={SearchIcon} /></Link>
                         <Link href={`/movies`}><HeaderItem title='MOVIES' Icon={FilmIcon} /></Link>
                         <Link href={`/tv`}><HeaderItem title='TV' Icon={VideoCameraIcon} /></Link>
                         <Link href={`/upcoming`}><HeaderItem title='UPCOMING' Icon={CalendarIcon} /></Link>
-                        <div className='relative'>
-                            <button onMouseEnter={handleDropdownOpen} onMouseLeave={handleDropdownClose} className="flex items-center focus:outline-none"><HeaderItem title='EXPLORE' Icon={MapIcon} /></button>
-                            {isDropdownOpen && (
-                                <div onMouseEnter={handleDropdownOpen} onMouseLeave={handleDropdownClose} className="absolute py-2 w-36 -mx-16 md:-mx-4 lg:mx-0 2xl:mx-2 3xl:mx-4 bg-primary border-2 text-center rounded-lg shadow-lg z-50">
-                                    <Link href={`/favorites`}><p className="py-2 text-lg font-medium text-white hover:bg-red-400">Favorites</p></Link>
-                                    <Link href={`/recommended`}><p className="py-2 text-lg font-medium text-white hover:bg-red-400">Recommended</p></Link>
-                                    <Link href={`/collections`}><p className="py-2 text-lg font-medium text-white hover:bg-red-400">Collections</p></Link>
-                                </div>
-                            )}
-                        </div>
+                        <Link href={`/favorites`}><HeaderItem title='FAVORITES' Icon={HeartIcon} /></Link>
+                        <Link href={`/recommended`}><HeaderItem title='RECOMMENDED' Icon={ThumbUpIcon} /></Link>
+                        <Link href={`/collections`}><HeaderItem title='COLLECTIONS' Icon={CollectionIcon} /></Link>
                     </div>
-                    <Image priority={true} className='object-contain' src={PeriLogo} alt='Peri' width={200} height={80} />
+                    <Image priority={true} className='object-contain overflow-hidden' src={PeriLogo} alt='Peri' width={200} height={80} />
                 </header>
             ) : (
                 <header> {/* Mobile Header */}
-                    <div onClick={() => setMobileMenu(!mobileMenu)} className="flex items-center justify-center mb-4">
+                    <div onClick={() => setMobileMenu(!mobileMenu)} className="flex items-center justify-center mb-8">
                         <MenuIcon className="h-10 text-red-400" />
                         <h2 className="text-4xl font-bold text-red-400 items-center">Peri</h2>
                     </div>
                     {mobileMenu && (
-                        <div className='h-full w-full fixed overflow-scroll z-50 bg-primary'>
-                            <div className="flex flex-col text-3xl font-bold gap-y-8 mx-8 animate-dropdown">
+                        <div className='h-full w-full fixed overflow-scroll scroll-gutter z-50 bg-primary'>
+                            <div className="flex flex-col text-3xl font-bold gap-y-8 mx-4 animate-dropdown">
                                 <Link href={`/`}>
                                     <div onClick={() => setMobileMenu(!mobileMenu)} className="flex items-center">
                                         <HomeIcon className='h-10' />
-                                        <p className='hover:text-red-400 ml-2'>Home</p>
+                                        <p className='hover:text-red-400 ml-2'>HOME</p>
                                     </div>
                                 </Link>
                                 <Link href={`/search`}>
                                     <div className="flex items-center">
                                         <SearchIcon className='h-10' />
-                                        <p className='hover:text-red-400 ml-2'>Search</p>
+                                        <p className='hover:text-red-400 ml-2'>SEARCH</p>
                                     </div>
                                 </Link>
                                 <Link href={`/movies`}>
                                     <div className="flex items-center">
                                         <FilmIcon className='h-10' />
-                                        <p className='hover:text-red-400 ml-2'>Movies</p>
+                                        <p className='hover:text-red-400 ml-2'>MOVIES</p>
                                     </div>
                                 </Link>
                                 <Link href={`/tv`}>
@@ -77,25 +66,25 @@ function Header() {
                                 <Link href={`/upcoming`}>
                                     <div className="flex items-center">
                                         <CalendarIcon className='h-10' />
-                                        <p className='hover:text-red-400 ml-2'>Upcoming</p>
+                                        <p className='hover:text-red-400 ml-2'>UPCOMING</p>
                                     </div>
                                 </Link>
                                 <Link href={`/favorites`}>
                                     <div className="flex items-center">
                                         <HeartIcon className='h-10' />
-                                        <p className='hover:text-red-400 ml-2'>Favorites</p>
+                                        <p className='hover:text-red-400 ml-2'>FAVORITES</p>
                                     </div>
                                 </Link>
                                 <Link href={`/recommended`}>
                                     <div className="flex items-center">
                                         <ThumbUpIcon className='h-10' />
-                                        <p className='hover:text-red-400 ml-2'>Recommended</p>
+                                        <p className='hover:text-red-400 ml-2'>RECOMMENDED</p>
                                     </div>
                                 </Link>
                                 <Link href={`/collections`}>
                                     <div className="flex items-center">
                                         <CollectionIcon className='h-10' />
-                                        <p className='hover:text-red-400 ml-2'>Collections</p>
+                                        <p className='hover:text-red-400 ml-2'>COLLECTIONS</p>
                                     </div>
                                 </Link>
                             </div>
--- components/Header/HeaderItem.js
+++ components/Header/HeaderItem.js
@@ -1,6 +1,6 @@
 function HeaderItem({Icon, title}) {
     return (
-        <div className="flex flex-col items-center cursor-pointer group -ml-1 md:ml-2 w-16 sm:w-20 md:w-24 lg:w-28 xl:w-32 2xl:w-36 3xl:w-40 text-white hover:text-red-400 link link-underline link-underline-black">
+        <div className="flex flex-col items-center cursor-pointer group -ml-2 md:ml-2 w-14 sm:w-16 md:w-20 lg:w-32 2xl:w-36 3xl:w-40 text-white hover:text-red-400 link link-underline link-underline-black">
             <Icon className="h-12 3xl:h-18 sm:group-hover:animate-bounce"/>
             <p className="opacity-0 group-hover:opacity-100 tracking-wide text-base md:text-xl mb-2">{title}</p>
         </div>
