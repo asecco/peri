d1b644bb23dfb16cf58e9e829e080aa9b58583c4
Andrew Secco <asecco99@gmail.com>
2023-08-10 00:11:19 -0400

    Added favorite icon to thumbnail when browsing

--- components/Layout/Thumbnail.js
+++ components/Layout/Thumbnail.js
@@ -1,14 +1,18 @@
 import Image from 'next/image';
+import { HeartIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
 import { useState } from 'react';
 import { BASE_URL } from "../../utils/constants";
 import { blurUrl } from '../../utils/helper';
+import { localStorageFavorites } from '../../utils/localStorage';
 import fallbackImage from '../../public/fallback.png';
 
 function Thumbnail({ result }) {
     const [isHovered, setIsHovered] = useState(false);
     const handleMouseEnter = () => setIsHovered(true);
     const handleMouseLeave = () => setIsHovered(false);
+
+    const { isFav, checkFav } = localStorageFavorites(result, result.media_type);
 
     const getRatingColor = (rating) => {
         if (rating <= 5.9) {
@@ -31,15 +35,18 @@ function Thumbnail({ result }) {
                 <div className='p-2 text-center' title={result.title || result.original_name || result.name}>
                     <h2 className='mt-1 text-xl lg:text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-normal group-hover:text-red-400 line-clamp-2 max-w-md'>{result.title || result.original_name || result.name}</h2>
                 </div>
-                {isHovered && !!result.vote_average && (
-                    <div className='absolute top-2 left-2'>
-                        <div className='relative w-10 h-10'>
-                            <div className={`absolute w-full h-full rounded-sm border-2 ${ratingColor}`}></div>
-                            <div className='absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4 text-white text-xl font-bold'>{Math.round(result.vote_average * 10) / 10}</div>
-                        </div>
-                    </div>
-                )}
             </Link>
+            {isHovered && !!result.vote_average && (
+                <div>
+                    <div className='absolute top-2 left-2 w-10 h-10'>
+                        <div className={`absolute w-full h-full rounded-sm border-2 ${ratingColor}`}></div>
+                        <div className='absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4 text-white text-xl font-bold'>{Math.round(result.vote_average * 10) / 10}</div>
+                    </div>
+                    <div onClick={checkFav} className='absolute top-2 right-2 w-10' title="Favorite">
+                        <HeartIcon className={isFav ? 'text-red-400 fill-red-400 hover:opacity-75' : 'hover:text-red-400 hover:fill-red-400'}/>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
